7608e89e850e4d5a0144bfa3c094a233f82bc530
ychoi <ychoi@DESKTOP-1OPUAHD>
2018-04-05 15:52:57 +0900

    예제

--- Example/src/example/test5.java
+++ Example/src/example/test5.java
@@ -1,21 +1,17 @@
 package example;
 
-public class For {
+public class test5 {
 
 	public static void main(String[] args) {
 		
-		int i;
-		int j;
-		
-		for (i=0; i<5; i++) {
-			for (j=0; j<=i; j++) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j <= i; j++) {
 				System.out.print("*");
 			}
-			System.out.println();
+			System.out.println(); // 4มู
 		}
 		
 		
-
 	}
 
 }
